097850b78f17346bdc3e94561eb77a0a4f5597c9
NamLeCS <nam.le94568@gmail.com>
2026-02-05 17:55:49 -0800

    Introduction Added
    Added a print statement that displays my introduction

--- labs/lab_1/lab_1a.py
+++ labs/lab_1/lab_1a.py
@@ -11,6 +11,6 @@ def main():
     name = "Nam Le" # TODO: Insert your name between the double quotes
 
     print(f"{name}, Welcome to the CSS course!")
-
+    print('Hello! My name is Nam. I am a sophomore from California. I enjoy badminton, golf, and drawing. My favorite ice cream flavor is raspberry cheesecake!')
 if __name__ == "__main__":
     main()
