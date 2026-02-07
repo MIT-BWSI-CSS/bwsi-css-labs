ef8c1a1ad7c61450f6bb4cd3d792be1e7a0624be
amybastola2 <amy.bastola2@gmail.com>
2026-02-07 13:26:07 -0800

    fixed long line length in comment docstring

--- labs/lab_1/lab_1b.py
+++ labs/lab_1/lab_1b.py
@@ -1,21 +1,25 @@
 """
 lab_1b.py
 
-This is a script that implements a simple calculator. It takes two numbers and an operation,
+This is a script that implements a simple calculator. 
+It takes two numbers and an operation,
 then performs the operation and returns the result. 
 
-The script asks the user to input the numbers and the operation to be performed,
+The script asks the user to input the numbers and 
+the operation to be performed,
 and prints the result to the terminal window.
 
 """
 
 def simple_calculator(operation: str, num1: float, num2: float) -> float:
     """
-    Function that takes in two numbers and an operation (add, subtract, multiply, divide),
+    Function that takes in two numbers and an operation 
+    (add, subtract, multiply, divide),
     then performs the operation on the two numbers and returns the result.
 
     Args:
-        operation (str): The operation to perform ("add", "subtract", "multiply", "divide").
+        operation (str): The operation to perform 
+        ("add", "subtract", "multiply", "divide").
         num1 (float): The first number.
         num2 (float): The second number.
 
