c00d049fc89f5c3b8d79bc29329be0285ccaf8c9
kijavnash <72349338+kijavnash@users.noreply.github.com>
2026-02-07 14:10:17 -0500

    Update lab_1b.py

--- labs/lab_1/lab_1b.py
+++ labs/lab_1/lab_1b.py
@@ -8,6 +8,12 @@ The script asks the user to input the numbers and the operation to be performed,
 and prints the result to the terminal window.
 
 """
+def is_float(str):
+    try:
+        float(str)
+        return True
+    except:
+        return False
 
 def simple_calculator(operation: str, num1: float, num2: float) -> float:
     """
@@ -42,13 +48,18 @@ def main():
     print(f"===== Simple Calculator =====")
 
     # Ask the user for sample input    
-    num1 = float(input("Enter the first number: "))
-    num2 = float(input("Enter the second number: "))
+    num1 = input("Enter the first number: ").strip()
+    num2 = input("Enter the second number: ").strip()
     operation = input("Enter the operation (add, subtract, multiply, divide): ").strip().lower()
 
     # Perform the calculation and display the result
-    result = simple_calculator(operation, num1, num2)
-    print(f"The result of {operation}ing {num1} and {num2} is: {result}")
+    
+    if is_float(num1) and is_float(num2):
+        result = simple_calculator(operation, float(num1), float(num2))
+        print(f"The result of {operation}ing {num1} and {num2} is: {result}")
+    else:
+        print("First and second number must be valid inputs.")
+    
 
 
 if __name__ == "__main__":
