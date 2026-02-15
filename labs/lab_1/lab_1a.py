58dec2215e61250ffd9ee9389785be302c5236ee
Yolanda Wang <mynameisyolandawang@gmail.com>
2026-02-14 19:20:54 -0800

    Added Introduction

--- labs/lab_1/lab_1a.py
+++ labs/lab_1/lab_1a.py
@@ -11,6 +11,7 @@ def main():
     name = "Yolanda Wang" # TODO: Insert your name between the double quotes
 
     print(f"{name}, Welcome to the CSS course!")
+    print("Hello :D My name is Yolanda and I'm from LA. I am currently building a rocket named Olaf yay")
 
 if __name__ == "__main__":
     main()
