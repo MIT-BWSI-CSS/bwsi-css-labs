8d133b73a2decf1bf77b6ee0e33209787d6978c7
paul-sopin <paul.sopin@gmail.com>
2026-02-15 12:41:32 -0800

    Add personal introduction

--- labs/lab_1/lab_1a.py
+++ labs/lab_1/lab_1a.py
@@ -11,6 +11,7 @@ def main():
     name = "Paul Sopin" # TODO: Insert your name between the double quotes
 
     print(f"{name}, Welcome to the CSS course!")
+    print("\nHey everyone! My name is Paul and I'm a sophomore from Dublin High School in California. This is my first year applying for the BWSI Summer Program, and I'm looking forward to building robots in RACECAR, and learning more about GitHub in this course. I already have limited experience with version control, but I'm interested in expanding my skills. In my free time, I play electric guitar, piano, and chess. Additionally, I like programming and weightlifting.")
 
 if __name__ == "__main__":
     main()
