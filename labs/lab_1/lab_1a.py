e75b008e8332f5c677753ae74620c347c07d53b6
bobrsultan <bobrsultan@gmail.com>
2026-02-15 15:32:48 -0500

    Add description

--- labs/lab_1/lab_1a.py
+++ labs/lab_1/lab_1a.py
@@ -3,14 +3,16 @@ lab_1a.py
 
 The first lab in the BWSI CSS course. To complete this lab, fill out the variable on line 10
 with your name. Then, save the code, add it to the staging area, and commit it to the Git tree.
+
 """
 
 def main():
     print("Hello World!")
 
     name = "Robert Sultan" # TODO: Insert your name between the double quotes
-
+    print("I am in tenth grade")
     print(f"{name}, Welcome to the CSS course!")
+    
 
 if __name__ == "__main__":
     main()
