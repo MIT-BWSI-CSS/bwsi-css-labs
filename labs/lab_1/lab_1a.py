ce6e5ecb44cf16bed70d4389a7ec1edf0ffd7120
Vansh-Singhal-12 <vansh12singhal@gmail.com>
2026-02-15 20:26:42 -0500

    Added my intro to lab_1a.py

--- labs/lab_1/lab_1a.py
+++ labs/lab_1/lab_1a.py
@@ -11,6 +11,6 @@ def main():
     name = "Vansh Singhal" # TODO: Insert your name between the double quotes
 
     print(f"{name}, Welcome to the CSS course!")
-
+    print("Hi all, my name is Vansh Singhal. I am a freshmen at Terra Environmental Research Institute in Miami, Florda. A fun fact about me is that I love to code and I am my school's debate team captain.")
 if __name__ == "__main__":
     main()
