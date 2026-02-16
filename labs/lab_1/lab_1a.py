8f37f391a7a6e91bc8bcbaf34b7c803500f03620
odurukkar <odurukkar@gmail.com>
2026-02-15 23:50:43 -0500

    Update lab_1a.py
    Added a introduction of myself

--- labs/lab_1/lab_1a.py
+++ labs/lab_1/lab_1a.py
@@ -11,6 +11,7 @@ def main():
     name = "Om Durukkar" # TODO: Insert your name between the double quotes
 
     print(f"{name}, Welcome to the CSS course!")
+    print("Hello all! My name is Om. I like STEM. My favorite subjects outside and inside of school are calculus, chemistry, computer science (particularly machine learning). I program competitively. I also cook food. I like to camp and enjoy the outdoors with my Scout troop.")
 
 if __name__ == "__main__":
     main()
