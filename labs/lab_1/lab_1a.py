e02f9b070c04f943c9900851a9785030180127fb
Brave Potato <kelliecpiano@gmail.com>
2026-02-17 21:22:14 -0800

    Added Self-Introduction

--- labs/lab_1/lab_1a.py
+++ labs/lab_1/lab_1a.py
@@ -11,6 +11,7 @@ def main():
     name = "Kellie Chung" # TODO: Insert your name between the double quotes
 
     print(f"{name}, Welcome to the CSS course!")
+    print("Hello hello! My name is Kellie, and I'm currently a sophomore in Nevada attending Davidson Academy Online! A fun fact about me is that I'm a member of an FRC team (987 Highrollers), and I've been working on my programming, which has been a super exciting journey! With BWSI, I'm really looking forward to potentially learning more about machine learning, large language models, and quantum computing :) I'm looking forward to learning more about computer science and systems in this course! Thank you so much and I can't wait to know you all better!")
 
 if __name__ == "__main__":
     main()
