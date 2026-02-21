ad180d365486a2d20ab1753604dc05cad17a9588
Enoch <enochkong5641@gmail.com>
2026-02-20 20:38:34 -0800

    Update lab_1a.py

--- labs/lab_1/lab_1a.py
+++ labs/lab_1/lab_1a.py
@@ -11,6 +11,7 @@ def main():
     name = "Enoch" # TODO: Insert your name between the double quotes
 
     print(f"{name}, Welcome to the CSS course!")
+    print("Hello! My name is Enoch Kong and I am a sophomore in San Francisco. Some of my hobbies is that I love playing badminton and tennis and I also play viola. I hope to meet all of you soon!")
 
 if __name__ == "__main__":
     main()
