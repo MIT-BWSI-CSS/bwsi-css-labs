5d13b6dc1fac3f0dd8d51edaec33acb82b8123c7
Jiarui (Jerry) Guo <jerryguo.me@gmail.com>
2026-02-21 15:51:35 -0500

    print personal message

--- labs/lab_1/lab_1a.py
+++ labs/lab_1/lab_1a.py
@@ -8,9 +8,9 @@ with your name. Then, save the code, add it to the staging area, and commit it t
 def main():
     print("Hello World!")
 
-    name = "" # TODO: Insert your name between the double quotes
+    name = "Jerry Guo" # TODO: Insert your name between the double quotes
 
-    print(f"{name}, Welcome to the CSS course!")
+    print("Hello all! I’m Jerry and I live in Harrison, NY. I enjoyed reading The Adventures of Tom Sawyer over the past summer and still re-read it from time to time. I love how Mark Twain blends history, nostalgia, and humor. Golf has also been a big part of my life, competitively for the past 10 years. I’m truly looking forward to becoming a better learner and coder from this course and you all!")
 
 if __name__ == "__main__":
     main()
