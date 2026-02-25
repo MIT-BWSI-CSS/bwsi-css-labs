0c22328f36d0e45bb7258941668315542136bd8d
SuperSwetter <110026929+SuperSwetter@users.noreply.github.com>
2026-02-24 18:10:29 -0600

    added to comment line
    added comment to explain variable purpose

--- labs/lab_1/lab_1a.py
+++ labs/lab_1/lab_1a.py
@@ -1,6 +1,8 @@
 """
 lab_1a.py
 
+This is to simulate a change made on a robot: robot_speed = 5 # m/s
+
 The first lab in the BWSI CSS course. To complete this lab, fill out the variable on line 10
 with your name. Then, save the code, add it to the staging area, and commit it to the Git tree.
 """
@@ -9,7 +11,7 @@ def main():
     print("Hello World!")
 
     name = "vir" # TODO: Insert your name between the double quotes
-
+    
     print(f"{name}, Welcome to the CSS course!")
     print("My name is Vir and I love to draw and code! d")
 if __name__ == "__main__":
