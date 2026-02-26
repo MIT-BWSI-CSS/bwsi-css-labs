f2fc966ac0c0314ff4973995641bca0511d4cf29
Caitlin C <cai.caitlin@gmail.com>
2026-02-25 17:06:49 -0800

    bye

--- labs/lab_1/lab_1a.py
+++ labs/lab_1/lab_1a.py
@@ -11,6 +11,7 @@ def main():
     name = "Caitlin" # TODO: Insert your name between the double quotes
 
     print(f"{name}, Welcome to the CSS course!")
+    print("Bye World!")
 
 if __name__ == "__main__":
     main()
