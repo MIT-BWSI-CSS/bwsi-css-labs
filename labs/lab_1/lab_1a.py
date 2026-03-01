bae23b12ed6e41e33ef6f13a5702d33aa0c33ba4
Dylan H. <topmaster4545@gmail.com>
2026-02-28 21:51:40 -0500

    Added bio

--- labs/lab_1/lab_1a.py
+++ labs/lab_1/lab_1a.py
@@ -11,6 +11,6 @@ def main():
     name = "Dylan Huang" # TODO: Insert your name between the double quotes
 
     print(f"{name}, Welcome to the CSS course!")
-
+    print(f"Hello everyone! I'm Dylan Huang, a sophomore from Massachusetts. I enjoy playing tennis and running! I'm looking forward to this course!")
 if __name__ == "__main__":
     main()
